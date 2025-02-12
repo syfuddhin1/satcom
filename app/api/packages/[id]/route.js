364d29e5205d758919f159d269f35de213d28e60
Soab Mahmud Syfuddhin <108150913+Soab42@users.noreply.github.com>
2025-02-13 02:29:07 +0600

    added prisma

--- app/api/packages/[id]/route.js
+++ app/api/packages/[id]/route.js
@@ -1,11 +1,6 @@
-import { packagesModel } from "@/models/package-model";
-import connectMongo from "@/services/mongo";
 import prisma from "@/prisma/db";
 
 export async function GET(request, { params }) {
-  const searchParams = request.nextUrl.searchParams;
-  const provider = searchParams.get("provider");
-
   const packDetails = await prisma.package.findUnique({
     where: { id: params.id },
   });
